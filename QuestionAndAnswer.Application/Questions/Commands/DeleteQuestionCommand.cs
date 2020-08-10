58fa6c7123fd54f685693d49e820659fecd3774a
ujin <ujin0505@gmail.com>
2020-08-10 19:25:43 +0300

    feat: Комманда удаления вопроса

---
+++ QuestionAndAnswer.Application/Questions/Commands/DeleteQuestionCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using QuestionAndAnswer.Application.Models;
+using QuestionAndAnswer.Data.Entities;
+using QuestionAndAnswer.Persistence;
+
+namespace QuestionAndAnswer.Application.Questions.Commands
+{
+    public class DeleteQuestionCommand: IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+    public class DeleteQuestionCommandHandler : IRequestHandler<DeleteQuestionCommand, bool>
+    {
+        private readonly IMediator _mediator;
+        private readonly ApplicationDbContext _dbContext;
+
+        public DeleteQuestionCommandHandler(IMediator mediator, ApplicationDbContext dbContext)
+        {
+            _mediator = mediator;
+            _dbContext = dbContext;
+        }
+        
+        public Task<bool> Handle(DeleteQuestionCommand request, CancellationToken cancellationToken)
+        {
+            var result = _dbContext.Questions.FirstOrDefault(q => q.Id == request.Id);
+            if (result == null)
+                return Task.FromResult(false);
+
+            _dbContext.Remove(result);
+            bool isSaved = _dbContext.SaveChanges() == 1;
+            return Task.FromResult(isSaved);
+        }
+    }
+}
