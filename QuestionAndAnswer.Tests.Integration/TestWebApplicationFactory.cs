8342120bde3eeb5bddb5d7a302b9b7f16b789adb
ujin <ujin0505@gmail.com>
2020-09-05 10:55:50 +0300

    feat: настроен запуск в docker

---
+++ QuestionAndAnswer.Tests.Integration/TestWebApplicationFactory.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QuestionAndAnswer.Persistence;
+
+namespace QuestionAndAnswer.Tests.Integration
+{
+    public class TestWebApplicationFactory: WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<ApplicationDbContext>));
+                services.Remove(descriptor);
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+                
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    /*var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();*/
+
+                    try
+                    {
+                        db.Database.EnsureCreated();
+                    }
+                    catch (Exception)
+                    {
+                        /*logger.LogError(ex, "An error occurred seeding the " +
+                                            "database with test messages. Error: {Message}", ex.Message);*/
+                    }
+                }
+            });
+        }
+    }
+}
--- QuestionAndAnswer/Program.cs
+++ QuestionAndAnswer/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using QuestionAndAnswer.Persistence;
 
 namespace QuestionAndAnswer
 {
@@ -13,20 +16,27 @@ namespace QuestionAndAnswer
     // 8. Automaper
     // 9. Polly
     // 10. Docker
-    
-    // 4. Async+ await + Task
-    // 5. OIDC
-    // 6. CORS
-    // 7. SSL + HTTPS
-    
+    // 11. Session/Cookie
     
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
